b2e6e019ab91204b6f28090647292d5c11393968
Mike <mike@mikejarosch.com>
2020-11-27 16:48:35 -0600

    Initial commit

---
+++ SwiftUI-BarcodeScanner/SwiftUI_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarcodeScannerApp.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Mike Jarosch on 11/27/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
