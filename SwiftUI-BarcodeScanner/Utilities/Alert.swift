2ac07daa9f98340c8cb0e1239cb7489f5ce43cd6
Mike <mike@mikejarosch.com>
2020-12-16 19:43:49 -0600

    Move Text from view into alert object

--- SwiftUI-BarcodeScanner/Utilities/Alert.swift
+++ SwiftUI-BarcodeScanner/Utilities/Alert.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
-                                              message: "Something is wrong with the camera. We are unable to capture the input.",
+    static let invalidDeviceInput = AlertItem(title: Text("Invalid Device Input"),
+                                              message: Text("Something is wrong with the camera. We are unable to capture the input."),
                                               dismissButton: .default(Text("Ok")))
-    static let invalidScannedType = AlertItem(title: "Invalid Scanned Type",
-                                              message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
+    static let invalidScannedType = AlertItem(title: Text("Invalid Scanned Type"),
+                                              message: Text("The value scanned is not valid. This app scans EAN-8 and EAN-13."),
                                               dismissButton: .default(Text("Ok")))
 }
